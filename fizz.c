b3d30de4dd784059242d05dc3ec28dcf1897c141
Shane Cowen <dopefish89@gmail.com>
2017-12-26 13:12:04 -0800

    Fix fizz.c and add fizz executable

--- fizz.c
+++ fizz.c
@@ -6,20 +6,20 @@ int main(int argc, char *argv[])
   int i, count;
 
   if(argc < 2) {
-    printf("You need more arguments\n", );
+    printf("You need 2 argument\n");
     exit(1);
   }
   count = atoi(argv[1]);
 
   for(i = 1; i <= count; i++) {
     if(i % 15 == 0) {
-      printf("Fizzbuzz\n\n", );
+      printf("Fizzbuzz\n");
     } else if(i % 5 == 0) {
-      printf("Buzz\n", );
+      printf("Buzz\n");
     } else if(i % 3 == 0) {
-      printf("Fizz\n", );
+      printf("Fizz\n");
     } else {
-      printf("^%d\n", );
+      printf("%d\n", i);
     }
   }
   return 0;
